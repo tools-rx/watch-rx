9c85ae8c60559447a16405369c55d0dc387327eb
Dave Baskin <dfbaskin@gmail.com>
2016-08-27 11:29:04 -0400

    Adding file watch add, rename, and delete tests.

--- tests/watch-rx.spec.js
+++ tests/watch-rx.spec.js
@@ -8,6 +8,8 @@ import {buildFileSet} from 'test-files-rx'
 import {getSubscriber, sortedFileList} from './test-helpers'
 
 const writeFileRx = Observable.bindNodeCallback(fs.writeFile)
+const renameRx = Observable.bindNodeCallback(fs.rename)
+const unlinkRx = Observable.bindNodeCallback(fs.unlink)
 
 const observeMaxTime = 1000
 const operationDelayTime = 750
@@ -94,6 +96,65 @@ describe('watch-rx', () => {
           performOperations(
             writeFileRx(localFileName('a/b/two'), 'test'),
             writeFileRx(localFileName('a/c/three'), 'test')
+          )
+        )
+        .takeUntil(Observable.timer(observeMaxTime))
+        .reduce(buildEventList, [])
+        .do(compareEventLists(expected))
+        .subscribe(getSubscriber(done))
+    })
+
+    it('should report file unlink', (done) => {
+      let expected = [
+        'unlink~a/b/two',
+        'unlink~a/c/three'
+      ]
+      Observable
+        .merge(
+          watchRx('**/*', { cwd: fileSet.localPath, ignoreInitial: true }),
+          performOperations(
+            unlinkRx(localFileName('a/b/two')),
+            unlinkRx(localFileName('a/c/three'))
+          )
+        )
+        .takeUntil(Observable.timer(observeMaxTime))
+        .reduce(buildEventList, [])
+        .do(compareEventLists(expected))
+        .subscribe(getSubscriber(done))
+    })
+
+    it('should report file add', (done) => {
+      let expected = [
+        'add~a/b/four',
+        'add~a/c/five'
+      ]
+      Observable
+        .merge(
+          watchRx('**/*', { cwd: fileSet.localPath, ignoreInitial: true }),
+          performOperations(
+            writeFileRx(localFileName('a/b/four'), 'test'),
+            writeFileRx(localFileName('a/c/five'), 'test')
+          )
+        )
+        .takeUntil(Observable.timer(observeMaxTime))
+        .reduce(buildEventList, [])
+        .do(compareEventLists(expected))
+        .subscribe(getSubscriber(done))
+    })
+
+    it('should report file rename', (done) => {
+      let expected = [
+        'unlink~a/b/one',
+        'unlink~a/c/three',
+        'add~a/b/one-renamed',
+        'add~a/c/three-renamed'
+      ]
+      Observable
+        .merge(
+          watchRx('**/*', { cwd: fileSet.localPath, ignoreInitial: true }),
+          performOperations(
+            renameRx(localFileName('a/b/one'), localFileName('a/b/one-renamed')),
+            renameRx(localFileName('a/c/three'), localFileName('a/c/three-renamed'))
           )
         )
         .takeUntil(Observable.timer(observeMaxTime))
