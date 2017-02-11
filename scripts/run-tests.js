8f4f6e85227db093df2b2e256fd68524cad103e9
Dave Baskin <dfbaskin@gmail.com>
2017-02-10 20:59:52 -0500

    feat(versions): Updating versions of dependencies.

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,14 +1,10 @@
 
 import Jasmine from 'jasmine'
-import Reporter from 'jasmine-spec-reporter'
+import {SpecReporter} from 'jasmine-spec-reporter'
 
 let jasmine = new Jasmine()
 jasmine.loadConfigFile('tests/jasmine.json')
-jasmine.addReporter(new Reporter({
-  isVerbose: false,
-  showColors: true,
-  includeStackTrace: false
-}))
+jasmine.addReporter(new SpecReporter())
 jasmine.onComplete(function (passed) {
   if (passed) {
     console.log('All tests passed.')
